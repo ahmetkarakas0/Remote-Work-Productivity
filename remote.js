5df12a853e61602f8e68d20c8d5f52ea9eee26a8
Ahmet Karakaş <karakasahmet.5@gmail.com>
2026-03-06 01:22:56 +0300

    Form and answer method has addded.

---
+++ remote.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.question-form');
+const result = document.querySelector('.result p');
+const resultheader = document.querySelector('.results span');
+const results = document.querySelector('.results');
+
+form.addEventListener('submit',e => {
+    const checkedValues = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value];
+    e.preventDefault();
+    let testPercent = 0;
+    checkedValues.forEach( answers => {
+        if( answers === 'N'){
+            testPercent += 20;
+        }
+        console.log(testPercent);
+    });
+    
+    result.textContent =`Your remote work productivity: %${testPercent}`;
+    resultheader.textContent =`%${testPercent}`;
+
+    results.classList.remove('d-none');
+    form.reset();
+
+})
