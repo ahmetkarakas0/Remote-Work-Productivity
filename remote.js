79ac23e149a6b71f90ccc76244947304de40e210
Ahmet Karakaş <karakasahmet.5@gmail.com>
2026-03-06 02:46:00 +0300

    Some animations has added to project

--- remote.js
+++ remote.js
@@ -2,6 +2,8 @@ const form = document.querySelector('.question-form');
 const result = document.querySelector('.result p');
 const resultheader = document.querySelector('.results span');
 const results = document.querySelector('.results');
+const rap = document.querySelector('.german');
+
 
 form.addEventListener('submit',e => {
     const checkedValues = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value];
@@ -13,12 +15,43 @@ form.addEventListener('submit',e => {
         }
         console.log(testPercent);
     });
-    
-    result.textContent =`Your remote work productivity: %${testPercent}`;
-    resultheader.textContent =`%${testPercent}`;
+    rap.textContent = "";
+    result.textContent =""; 
 
     results.classList.remove('d-none');
     form.reset();
     window.scroll(0,0);
+    setTimeout(() => {
+        result.textContent =`Your remote work productivity: %${testPercent}`;  
+
+    }, 3502);
+
+    let messageMy = "Your remote work productivity"
+    let looper = 0;
+    const daktilo = setInterval(() => {
+        rap.textContent += messageMy[looper];
+        looper++;
+        if(looper == messageMy.length){
+            clearInterval(daktilo);
+        }
+    },32)
+
+    if(testPercent === 0) {
+        resultheader.textContent =`%${testPercent}`;
+        return;
+    }
+    
+    let percentFirst = 0;
+    const percentFunction = setInterval( () => {
+        percentFirst++;
+        resultheader.textContent =`%${percentFirst}`;
+        if (percentFirst == testPercent) {
+            clearInterval(percentFunction);
+        }
+
+
+
+    },21)
+
 
 })
